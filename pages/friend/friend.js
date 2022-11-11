0cc8593b4f73a69a0903c912b737c8f0f2c7a9a4
Exungsh <77920395+Exungsh@users.noreply.github.com>
2022-11-11 17:29:15 +0800

    完善“关注”点击效果

--- pages/friend/friend.js
+++ pages/friend/friend.js
@@ -1,65 +1,244 @@
 // pages/friend/friend.js
+
+const app=getApp()
+
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    datalist: [
-      [{
-        'id': 0,
-        'name': 'girl0',
-        'intro': 'intro0',
-        'friend_tag': ['0_tag1', '0_tag2', '0_tag3']
-      }, {
-        'id': 1,
-        'name': 'girl1',
-        'intro': 'intro1',
-        'friend_tag': ['1_tag1', '1_tag2', '1_tag3']
-      }],
-      [{
-        'id': 0,
-        'name': 'boy0',
-        'intro': 'intro0',
-        'friend_tag': ['0_tag1', '0_tag2', '0_tag3']
-      }]
-    ],
-    friendlist:[]
+    flag: -1,
+    is_friend: false,
+    datalist1: [],
+    datalist2: [],
+    friendlist: []
+  },
+  is_myfriend(_id) {
+    this.is_friend=false
+    for(var i=0;i<app.globalData.my_friends.length;++i) {
+      if(_id==app.globalData.my_friends[i]) {
+        // console.log(app.globalData.my_friends[i])
+        this.is_friend=true
+        app.globalData.my_friends.splice(i,1)
+      }
+    }
+  },
+  follow(e) {
+    wx.cloud.init({
+      env: 'cloud1-3gbbimin78182c5d'
+    })
+    const db = wx.cloud.database()
+    const _ = db.command
+    var _id=e.currentTarget.dataset._id
+    var num=e.currentTarget.dataset.num
+    console.log(num)
+    if(this.data.flag==1) {
+      this.data.datalist1[num].follow*=-1
+      console.log(this.data.datalist1[num].follow)
+      this.setData({
+        friendlist: this.data.datalist1,
+        nav1bgc: "#f0f9f6",
+        nav1color: "#588c7e",
+        nav2bgc: "#588c7e",
+        nav2color: "#f0f9f6",
+      })
+      if(this.data.datalist1[num].follow==1){
+        wx.showToast({
+          title: '已关注',
+          icon: 'success',
+          duration: 1000
+        })
+      }
+      else{
+        wx.showToast({
+          title: '已取消关注',
+          icon: 'success',
+          duration: 1000
+        })
+      }
+      for(var i=0;i<this.data.datalist2.length;++i) {
+        if(_id==this.data.datalist2[i].id) {
+          this.data.datalist2[i].follow=this.data.datalist1[num].follow
+        }
+      }
+    }
+    else {
+      this.data.datalist2[num].follow*=-1
+      console.log(this.data.datalist2[num].follow)
+      this.setData({
+        friendlist: this.data.datalist2,
+        nav1color: "#f0f9f6",
+        nav1bgc: "#588c7e",
+        nav2bgc: "#f0f9f6",
+        nav2color: "#588c7e",
+      })
+      if(this.data.datalist2[num].follow==1){
+        wx.showToast({
+          title: '已关注',
+          icon: 'success',
+          duration: 1000
+        })
+      }
+      else{
+        wx.showToast({
+          title: '已取消关注',
+          icon: 'success',
+          duration: 1000
+        })
+      }
+      for(var i=0;i<this.data.datalist1.length;++i) {
+        if(_id==this.data.datalist1[i].id) {
+          this.data.datalist1[i].follow=this.data.datalist2[num].follow
+        }
+      }
+    }
+    this.is_myfriend(_id)
+    setTimeout(()=>{
+      if(this.is_friend==false) {
+        app.globalData.my_friends.push(_id)
+      }
+    },200)
+    setTimeout(()=>{
+      console.log(app.globalData.my_friends)
+      db.collection('user').where({
+        _id: app.globalData.my_id
+      })
+      .update({
+        data: {
+          friends: _.set(app.globalData.my_friends)
+        }
+      })
+    },500)
+  },
+  update_recommend() {
+    wx.cloud.init({
+      env: 'cloud1-3gbbimin78182c5d'
+    })
+    const db = wx.cloud.database()
+    const _ = db.command
+    const $ = db.command.aggregate
+    let my_tag=app.globalData.my_tags
+    var array=[]
+
+    db.collection('user').where({
+      _id:_.neq(app.globalData.my_id).and(_.nin(app.globalData.my_hmd)).and(_.nin(app.globalData.my_friends)),
+      isfind:_.eq(true),
+      friend_tag: _.all([my_tag[0]]).or(_.all([my_tag[1]])).or(_.all([my_tag[2]])).or(_.all([my_tag[3]])).or(_.all([my_tag[4]])).or(_.all([my_tag[5]]))
+    })
+    .get({
+      success: function(res) {
+        for(var j=0;j<res.data.length;++j) {
+          var friend= {
+            'id': res.data[j]._id,
+            'name': res.data[j].name,
+            'intro': res.data[j].intro,
+            'friend_tag': res.data[j].tags,
+            'head': res.data[j].head_img,
+            'num':j,
+            'follow':-1
+          }
+          array.push(friend)
+        }
+      }
+    })
+    setTimeout(()=>{
+      this.setData({
+        datalist1: array,
+        nav1bgc: "#f0f9f6",
+        nav1color: "#588c7e",
+        nav2bgc: "#588c7e",
+        nav2color: "#f0f9f6",
+      })
+    },500)
   },
   recommend() {
+    this.data.flag*=-1
     this.setData({
-      friendlist: this.data.datalist[0],
+      friendlist: this.data.datalist1,
       nav1bgc: "#f0f9f6",
       nav1color: "#588c7e",
       nav2bgc: "#588c7e",
       nav2color: "#f0f9f6",
     })
   },
+  update_recent() {
+    wx.cloud.init({
+      env: 'cloud1-3gbbimin78182c5d'
+    })
+    const db = wx.cloud.database()
+    const _ = db.command
+    const $ = db.command.aggregate
+    let my_tag=app.globalData.my_tags
+    var activitiy_id=Object.keys(app.globalData.my_activities)
+    var activitiy_table=Object.values(app.globalData.my_activities)
+    var recent_id=[]
+    var array=[]
+    
+    for(var i=0;i<activitiy_id.length;++i){
+      db.collection(activitiy_table[i]).where({
+        _id: activitiy_id[i]
+      })
+      .get({
+        success:(res)=>{
+          recent_id.push.apply(recent_id,Object.keys(res.data[0].members[0]))
+        }
+      })
+    }
+    setTimeout(()=>{
+      db.collection('user').where({
+        _id:_.neq(app.globalData.my_id).and(_.nin(app.globalData.my_hmd)).and(_.nin(app.globalData.my_friends)).and(_.in(recent_id)),
+        isfind:_.eq(true)
+      })
+      .get({
+        success:(res)=>{
+          for(var j=0;j<res.data.length;++j) {
+            var friend= {
+              'id': res.data[j]._id,
+              'name': res.data[j].name,
+              'intro': res.data[j].intro,
+              'friend_tag': res.data[j].tags,
+              'head': res.data[j].head_img,
+              'num':j,
+              'follow':-1
+            }
+            array.push(friend)
+          }
+        }
+      })
+    },500)
+    setTimeout(()=>{
+      this.setData({
+        datalist2: array
+      })
+    },1000)
+  },
   recent() {
+    this.data.flag*=-1
     this.setData({
-      friendlist: this.data.datalist[1],
+      friendlist: this.data.datalist2,
       nav1color: "#f0f9f6",
       nav1bgc: "#588c7e",
       nav2bgc: "#f0f9f6",
       nav2color: "#588c7e",
     })
   },
+  
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    this.update_recommend()
+    this.update_recent()
+    setTimeout(()=>{
+      this.recommend()
+    },500)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-    this.setData({
-      friendlist: this.data.datalist[0],
-      nav1bgc: "#f0f9f6",
-      nav1color: "#588c7e",
-      nav2bgc: "#588c7e",
-      nav2color: "#f0f9f6",
-    })
+    // this.recommend();
   },
 
   /**
